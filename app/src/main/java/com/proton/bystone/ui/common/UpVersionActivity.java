76efa4676d19f85aeb873623d12eef9f68b1b745
343887511@qq.com <ailgxkjy198687>
2016-08-01 14:48:42 +0800

    add upgrade

---
+++ app/src/main/java/com/proton/bystone/ui/common/UpVersionActivity.java
@@ -0,0 +1,24 @@
+package com.proton.bystone.ui.common;
+
+import android.os.Bundle;
+
+import com.proton.bystone.R;
+import com.proton.library.ui.MTFBaseActivity;
+import com.proton.library.ui.annotation.MTFActivityFeature;
+
+/**
+ * 软件检测更新
+ */
+@MTFActivityFeature(layout = R.layout.activity_up_version)
+public class UpVersionActivity extends MTFBaseActivity {
+
+    @Override
+    public void initialize(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void backPressed() {
+
+    }
+}
